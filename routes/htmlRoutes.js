6d26ae6c3b43c5842ebe336fa39d7c7608091f71
Ver <vpasamic22@gmail.com>
2020-10-29 21:05:54 -0700

    revert

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,14 +2,14 @@ var path = require("path");
 
 module.exports = function(app) {
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
   
 app.get("/exercise", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
   
 app.get("/stats", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
  });
 };
